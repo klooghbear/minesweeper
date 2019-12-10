d67e89f42ae5d43bcf014c629347c1c280224a58
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2019-12-10 15:46:18 +1300

    Complete check for winner

--- minesweeper.js
+++ minesweeper.js
@@ -68,6 +68,8 @@ function startGame () {
   }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
+  document.addEventListener('click', checkForWin)
+  document.addEventListener('contextmenu', checkForWin)
 }
 
 // Define this function to look for a win condition:
@@ -76,10 +78,18 @@ function startGame () {
 // 2. Are all of the mines marked?
 function checkForWin () {
 
+  for(i = 0; i < board.cells.length; i++){
+    if(board.cells[i].isMine == true && board.cells[i].isMarked == false){
+      return
+    }
+    if(board.cells[i].isMine == false  && board.cells[i].hidden){
+      return
+    } 
+  }
   
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  lib.displayMessage('You win!')
 }
 
 // Define this function to count the number of mines around the cell
